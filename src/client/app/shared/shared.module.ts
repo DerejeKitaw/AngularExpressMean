25ac416f6a67ae27cd8bbcb528fbbe5a406a7d59
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-10-01 02:14:38 -0400

    toaste component created and declared

--- src/client/app/shared/shared.module.ts
+++ src/client/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingComponent } from './loading/loading.component';
+import { ToastComponent } from './toast/toast.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LoadingComponent]
+  declarations: [LoadingComponent, ToastComponent]
 })
 export class SharedModule { }
