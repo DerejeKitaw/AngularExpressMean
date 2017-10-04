646b4984667737c39bfb70063f264eedbed2f583
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-10-03 23:36:23 -0400

    format changed

--- src/client/app/services/user.service.ts
+++ src/client/app/services/user.service.ts
@@ -7,10 +7,10 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class UserService {
 
-  private headers = new Headers({ 'Content-Type': 'application/json', 'charset': 'UTF-8' });
-  private options = new RequestOptions({ headers: this.headers });
+    private headers = new Headers({ 'Content-Type': 'application/json', 'charset': 'UTF-8' });
+    private options = new RequestOptions({ headers: this.headers });
 
-  constructor(private http: Http) { }
+    constructor(private http: Http) { }
   
     register(user): Observable<any> {
       return this.http.post('/api/user', JSON.stringify(user), this.options);
