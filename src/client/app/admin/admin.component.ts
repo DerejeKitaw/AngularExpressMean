9aa83206303fcf2a60a7c24223bdadeacc484f6a
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-10-05 22:47:25 -0400

    admin implemented

--- src/client/app/admin/admin.component.ts
+++ src/client/app/admin/admin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastComponent } from '../shared/toast/toast.component';
+import { UserService } from '../services/user.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,14 +9,32 @@ import { ToastComponent } from '../shared/toast/toast.component';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+  users = [];
+  isLoading = true;
 
-  isLoading=true;
-  constructor(
-    public toast:ToastComponent
-  ) { }
-
+  constructor(public auth: AuthService,
+              public toast: ToastComponent,
+              private userService: UserService) { }
+  
   ngOnInit() {
-    this.toast.setMessage('Admin','success')
+    this.toast.setMessage('Admin','success');
+    // get all users loged in befor the component load
+    this.getUsers();
+  }
+  
+  getUsers() {
+    this.userService.getUsers().subscribe(
+      data => this.users = data,
+      error => console.log(error),
+      () => this.isLoading = false
+    );
   }
 
+  deleteUser(user) {
+    this.userService.deleteUser(user).subscribe(
+      data => this.toast.setMessage('user deleted successfully.', 'success'),
+      error => console.log(error),
+      () => this.getUsers()
+    );
+  }
 }
