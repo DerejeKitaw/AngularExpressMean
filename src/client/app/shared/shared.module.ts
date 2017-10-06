dea1ff31897ce0c684470aa7f14b9163fe9f1e25
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-10-05 21:41:27 -0400

    export Modules to access from other component

--- src/client/app/shared/shared.module.ts
+++ src/client/app/shared/shared.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingComponent } from './loading/loading.component';
 import { ToastComponent } from './toast/toast.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   imports: [
     CommonModule
+  ],
+  exports: [ //export Modules to access from other component
+    // Shared Modules
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    // Shared Components
+    ToastComponent,
+    LoadingComponent
   ],
   declarations: [LoadingComponent, ToastComponent]
   ,
