94fda3e6c39fd97c1f1443d2a02ca26736e09212
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-10-05 21:13:50 -0400

    loading component added

--- src/client/app/shared/loading/loading.component.ts
+++ src/client/app/shared/loading/loading.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.scss']
 })
-export class LoadingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class LoadingComponent {
+  @Input() condition: boolean;
 }
